e51ec671edacca29ee497f05728e60fb1d5e5114
Oliver Kulas <kulasoliver@gmail.com>
2015-09-10 10:27:09 +0200

    Merge remote-tracking branch 'origin/dev_master' into dev_master
    Conflicts:
    CS_CompanionApp-csharp.sln
    CS_CompanionApp.sln

---
+++ Assets/Scripts/Editor/WbToOliBox.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class WbToOliBox : ScriptableWizard {
+	
+	public int amount = 10;
+	public string namePrefix = "EVOI_Site_";
+	
+	//-------------------------------------------------------------------------
+	
+	[MenuItem("WbEditorTools/Olis Toolbox/Create Empty SiteObjects")]
+	static void CreateWizard() {
+		ScriptableWizard.DisplayWizard<WbToOliBox>("Olis Toolbox", "Create", "Apply");
+	}
+	
+	//-------------------------------------------------------------------------
+	
+	void OnWizardCreate() {
+	}
+	
+	//-------------------------------------------------------------------------
+	
+	void OnWizardUpdate() {
+	}
+	
+	//-------------------------------------------------------------------------
+	
+	void OnWizardOtherButton() {
+		
+		if(Selection.activeTransform != null){
+			for (int i = 1; i <= this.amount; i++) {
+				if (i < 100) {
+					
+				}
+				GameObject go = new GameObject(namePrefix + i);
+				go.transform.parent = Selection.activeTransform;
+			}
+		} else {
+			Debug.LogError("No parent selected.");
+		}
+		
+	}
+}
